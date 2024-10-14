8696c3e52892f5f56028a85d1e5fe72280b4a033
GraceKafuu <gracekafuu@gmail.com>
2024-10-14 13:41:56 +0800

    create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gkfutils",
+    version="1.0.7",
+    author="GraceKafuu",
+    author_email="gracekafuu@gmail.com",
+    description="GraceKafuu utils",
+    long_description=long_description,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

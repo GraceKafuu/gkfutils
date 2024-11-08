af9ed3ed9167c3905a76cb0fe67e559076e81eeb
GraceKafuu <gracekafuu@gmail.com>
2024-11-08 09:20:49 +0800

    update

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import setuptools
 
 setuptools.setup(
     name="gkfutils",
-    version="1.1.0",
+    version="1.1.1",
     author="GraceKafuu",
     author_email="gracekafuu@gmail.com",
     description="GraceKafuu utils",

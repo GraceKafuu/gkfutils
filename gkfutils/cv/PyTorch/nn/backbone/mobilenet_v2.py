c4773109911519cdc9e1c68ce4ed867156c09252
GraceKafuu <gracekafuu@gmail.com>
2024-11-26 15:42:40 +0800

    update

--- gkfutils/cv/PyTorch/nn/backbone/mobilenet_v2.py
+++ gkfutils/cv/PyTorch/nn/backbone/mobilenet_v2.py
@@ -1,3 +1,16 @@
+# from functools import partial
+# from typing import Any, Callable, List, Optional
+
+# import torch
+# from torch import nn, Tensor
+
+# from ..ops.misc import Conv2dNormActivation
+# from ..transforms._presets import ImageClassification
+# from ..utils import _log_api_usage_once
+# from ._api import register_model, Weights, WeightsEnum
+# from ._meta import _IMAGENET_CATEGORIES
+# from ._utils import _make_divisible, _ovewrite_named_param, handle_legacy_interface
+
 from functools import partial
 from typing import Any, Callable, List, Dict, Optional, Sequence, Tuple, Union, TypeVar
 import torch

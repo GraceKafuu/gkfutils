cbb93e06a789b7014439694bf9673dd2e79b2e98
GraceKafuu <gracekafuu@gmail.com>
2025-03-26 14:55:57 +0800

    Update

--- gkfutils/cv/tcp/client.py
+++ gkfutils/cv/tcp/client.py
@@ -1,24 +1,47 @@
 import socket
+import threading
+
+
+class TCP_Client(object):
+    def __init__(self, server_host="127.0.0.1", server_port=12345):
+        self.server_host = server_host
+        self.server_port = server_port
+
+        # 创建 TCP 客户端
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # 连接服务端
+        self.server_address = (self.server_host, self.server_port)
+        self.client_socket.connect(self.server_address)
+        print(f"服务端 {self.server_address} 连接成功！")
+
+    def run(self):
+        while True:
+            # 发送数据到服务端
+            message = input("请输入要发送的消息: ")
+            self.client_socket.send(message.encode("utf-8"))
+            if not message:
+                break
+
+            # 接收服务端的响应
+            response = self.client_socket.recv(1024).decode("utf-8")
+            print(f"服务端回复: {response}")
+
+        # 关闭连接
+        self.close()
+
+    def start_run(self):
+        try:
+            # thread.start_new_thread(self.run, ())
+            t = threading.Thread(target=self.run)
+            t.start()
+        except Exception as e:
+            print("Error: 无法启动线程 {}".format(e))
+
+    def close(self):
+        self.client_socket.close()
+
 
 if __name__ == "__main__":
-    # 创建 TCP 客户端
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    # 连接服务端
-    server_address = ("127.0.0.1", 12345)
-    client_socket.connect(server_address)
-    print(f"服务端 {server_address} 连接成功！")
-
-    while True:
-        # 发送数据到服务端
-        message = input("请输入要发送的消息: ")
-        client_socket.send(message.encode("utf-8"))
-        if not message:
-            break
-
-        # 接收服务端的响应
-        response = client_socket.recv(1024).decode("utf-8")
-        print(f"服务端回复: {response}")
-
-    # 关闭连接
-    client_socket.close()
+    tcp_client = TCP_Client(server_host="127.0.0.1", server_port=12345)
+    tcp_client.start_run()
--- gkfutils/cv/tcp/server.py
+++ gkfutils/cv/tcp/server.py
@@ -1,36 +1,61 @@
 import socket
+import threading
+
+
+class TCP_Server(object):
+    def __init__(self, host="127.0.0.1", port=12345):
+        self.host = host
+        self.port = port
+
+        # 创建 TCP 服务端
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # 绑定 IP 地址和端口
+        self.server_address = (self.host, self.port)  # 绑定本机 IP 和端口 12345
+        self.server_socket.bind(self.server_address)
+
+        # 监听客户端连接
+        self.server_socket.listen(5)  # 最大连接数为 5
+        print(f"服务端 {self.server_address} 已启动, 等待客户端连接...")
+
+    def run(self):
+        while True:
+            try:
+                # 接受客户端连接
+                self.client_socket, self.client_address = self.server_socket.accept()
+                print(f"客户端 {self.client_address} 连接成功！")
+
+                while True:
+                    # 接收客户端发送的数据
+                    data = self.client_socket.recv(1024).decode("utf-8")
+                    if not data:
+                        break
+                    print(f"收到客户端发送的消息: {data}")
+
+                    # 发送数据给客户端
+                    response = f"服务端已收到你的消息: {data}"
+                    self.client_socket.send(response.encode("utf-8"))
+
+                # 关闭连接
+                self.client_socket.close()
+                self.close()
+            except Exception as e:
+                print(e)
+
+    def start_run(self):
+        try:
+            # thread.start_new_thread(self.run, ())
+            t = threading.Thread(target=self.run)
+            t.start()
+        except Exception as e:
+            print("Error: 无法启动线程 {}".format(e))
+
+    def close(self):
+        self.server_socket.close()
 
 
 if __name__ == "__main__":
-    # 创建 TCP 服务端
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    # 绑定 IP 地址和端口
-    server_address = ("127.0.0.1", 12345)  # 绑定本机 IP 和端口 12345
-    server_socket.bind(server_address)
-
-    # 监听客户端连接
-    server_socket.listen(5)  # 最大连接数为 5
-    print(f"服务端 {server_address} 已启动, 等待客户端连接...")
-
-    while True:
-        # 接受客户端连接
-        client_socket, client_address = server_socket.accept()
-        print(f"客户端 {client_address} 连接成功！")
-
-        while True:
-            # 接收客户端发送的数据
-            data = client_socket.recv(1024).decode("utf-8")
-            if not data:
-                break
-            print(f"收到客户端消息: {data}")
-
-            # 发送数据给客户端
-            response = f"服务端已收到你的消息: {data}"
-            client_socket.send(response.encode("utf-8"))
-
-        # 关闭连接
-        client_socket.close()
-        server_socket.close()
+    tcp_server = TCP_Server(host="127.0.0.1", port=12345)
+    tcp_server.start_run()
 
 
